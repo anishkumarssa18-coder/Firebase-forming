2bc3caa2bd1ee1936fed2ea3676fd3924714d612
Anish Kumar <anishkumarssa18@gmail.com>
2025-09-28 11:09:20 +0000

    Error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, FirebaseOptions } from 'firebase/app';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, signOut, onAuthStateChanged, User } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getOrInitializeAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig: FirebaseOptions = {
   projectId: 'studio-2775585837-1c446',
   appId: '1:1091828136369:web:bf6ffaf925b38062a4e590',
-  apiKey: 'AIzaSyA15GPBJU-MENBpI1yR2NLzs-fd9QDLf1Y',
+  apiKey: 'AIzaSyAvjpwaSfwJsK23RKwLJxqwQERM6eJVnd4',
   authDomain: 'studio-2775585837-1c446.firebaseapp.com',
   measurementId: '',
   messagingSenderId: '1091828136369',
@@ -20,5 +19,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-
-export { app, auth, db, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, signOut, onAuthStateChanged, type User };
+export { app, auth, db };
